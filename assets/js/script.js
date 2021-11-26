3dd27ebccdebd67bdf89bc6c17a695548a4b41f8
polizoto <polizoto@hotmail.com>
2021-11-26 17:18:19 -0500

    Correct broken relative paths to JS and CSS; minor CSS edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
